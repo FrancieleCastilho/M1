7ca8a03f74134e3e019c3aceff3b7fbc1eff6e94
Franciele <francielecasteves@gmail.com>
2018-10-24 18:23:47 -0300

    Ola Mundo

---
+++ app/code/local/Academia/HelloWorld/controllers/IndexController.php
@@ -0,0 +1,12 @@
+<?php
+
+class Academia_HelloWorld_IndexController extends Mage_Core_Controller_Front_Action{
+    public function indexAction(){
+            echo "Hello Academia!";
+    }
+
+    public function olaAction(){
+        echo "Ola";
+    }
+}
+
---
+++ app/code/local/FCamara/OlaMundo/controllers/IndexController.php
@@ -0,0 +1,10 @@
+<?php
+
+class FCamara_OlaMundo_IndexController extends Mage_Core_Controller_Front_Action {
+    public function IndexAction(){
+        echo "Olá FCamara!";
+    }
+    public function OlaAction(){
+        echo "Olá Alunos!";
+    }
+}
